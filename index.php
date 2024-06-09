224a82af7e42febee80ffd8354f7df58998d4260
VladUngurean <136657499+VladUngurean@users.noreply.github.com>
2024-06-10 01:26:30 +0300

    chat secction stylng

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
         <header>
 
             <div class="header_top">
-                <img class="logo" src="" alt="">
+                <span class="logo">Blablabla</span>
                 <div class="social_media">
                     <img src="/images/header/IG.SVG" alt="">
                     <img src="/images/header/TG.SVG" alt="">
@@ -45,6 +45,8 @@
         <div id="content_main" class="content_most_popular">
 
             <div class="most_popular_post">
+                <h1>Самые популярные темы</h1>
+                <p>С нами вы можете узнать больше.</p>
                 <img class="most_popular_post_img" src="https://i.postimg.cc/9fnXGD2W/1.jpg" alt="">
                 <div class="most_popular_post_overlay">
                     <div class="most_popular_post_overlay-text">Palm Beach <br><span>Florida</span></div>
@@ -68,19 +70,96 @@
 
         </div>
 
-        <!-- content section 1 end -->
-
-        <!-- content section 2 start -->
-        <!-- content section 2 end -->
-
-        <!-- content section 3 start -->
-        <!-- content section 3 end -->
         <div id="content_side">
-
+            <a href="">
+                <div class="content_side_wraper">
+                    <img src="" alt="">
+                    <div class="content_side_title">
+                        <h3>Lorem ipsum dolor sit amet consectetur. Tellus a ac metus </h3>
+                        <p class="">Туризм</p>
+                    </div>
+                </div>
+            </a>
+            <a href="">
+                <!-- <hr> -->
+                <div class="content_side_wraper">
+                    <img src="" alt="">
+                    <div class="content_side_title">
+                        <h3>Lorem ipsum dolor sit amet consectetur. Tellus a ac metus </h3>
+                        <p class="">Туризм</p>
+                    </div>
+                </div>
+            </a>
+            <a href="">
+                <!-- <hr> -->
+                <div class="content_side_wraper">
+                    <img src="" alt="">
+                    <div class="content_side_title">
+                        <h3>Lorem ipsum dolor sit amet consectetur. Tellus a ac metus </h3>
+                        <p class="">Туризм</p>
+                    </div>
+                </div>
+            </a>
+            <a href="">
+                <!-- <hr> -->
+                <div class="content_side_wraper">
+                    <img src="" alt="">
+                    <div class="content_side_title">
+                        <h3>Lorem ipsum dolor sit amet consectetur. Tellus a ac metus </h3>
+                        <p class="">Туризм</p>
+                    </div>
+                </div>
+            </a>
+            <a href="">
+                <!-- <hr> -->
+                <div class="content_side_wraper">
+                    <img src="" alt="">
+                    <div class="content_side_title">
+                        <h3>Lorem ipsum dolor sit amet consectetur. Tellus a ac metus </h3>
+                        <p class="">Туризм</p>
+                    </div>
+                </div>
+            </a>
         </div>
     </section>
-    <!-- content section end -->
+    <!-- content section 1 end -->
 
+    <!-- content section 2 start -->
+    <section class="section section_content section_content_2">
+        <!-- content section 1 start -->
+        <div id="content_main" class="content_most_popular">
+
+            <div class="most_popular_post">
+                <h1>Самые популярные темы</h1>
+                <p>С нами вы можете узнать больше.</p>
+                <img class="most_popular_post_img" src="https://i.postimg.cc/9fnXGD2W/1.jpg" alt="">
+                <div class="most_popular_post_overlay">
+                    <div class="most_popular_post_overlay-text">Palm Beach <br><span>Florida</span></div>
+                </div>
+            </div>
+
+            <div id="most_popular_post-small">
+                <div class="most_popular_post">
+                    <img class="most_popular_post_img" src="https://i.postimg.cc/9fnXGD2W/1.jpg" alt="">
+                    <div class="most_popular_post_overlay">
+                        <div class="most_popular_post_overlay-text">Palm Beach <br><span>Florida</span></div>
+                    </div>
+                </div>
+                <div class="most_popular_post">
+                    <img class="most_popular_post_img" src="https://i.postimg.cc/9fnXGD2W/1.jpg" alt="">
+                    <div class="most_popular_post_overlay">
+                        <div class="most_popular_post_overlay-text">Palm Beach <br><span>Florida</span></div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+    <!-- content section 2 end -->
+
+    <!-- content section 3 start -->
+    <!-- content section 3 end -->
 
     <!-- <div class="language-buttons">
         <button data-lang="ru" onclick="setLanguage('ru')">Русский</button>
